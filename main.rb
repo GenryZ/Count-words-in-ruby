dbc713f60faebfe237ed145ae956159da07527b6
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-03-03 16:43:23 +0300

    Add all words in hash and count them

--- main.rb
+++ main.rb
@@ -1,8 +1,23 @@
 f = File.open "text.txt", "r"
 
+@hh = {}
+
+def add_to_hash word
+  if !word.empty?
+    word.downcase!
+
+    cnt = @hh[word].to_i
+    cnt += 1
+    @hh[word] = cnt
+  end
+end
+
 f.each_line do |line|
-  arr = line.split(/ /)
-  p arr
+  arr = line.split(/\s|\n|,/) #\s whitespace | or 
+  arr.each {|word| add_to_hash(word)}
 end
 
 f.close
+@hh.each do |key, value|
+  puts  "#{value} #{key}"
+end
